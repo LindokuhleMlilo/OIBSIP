7520278624028b9179fb57810a3493546d9497d1
Lindokuhle Mlilo <lmlilo023@student.wethinkcode.co.za>
2024-06-26 18:42:05 +0200

    generated random numbers from 1 to 100

--- Number_Guessing_Game/src/main/java/org/Game/Main.java
+++ Number_Guessing_Game/src/main/java/org/Game/Main.java
@@ -8,14 +8,27 @@ public class Main {
 
         Random rand = new Random();
 
-        int randomNumber = rand.nextInt(100) + 1;
 
-        System.out.println("Enter your guess between (1 - 100)");
+            int randomNumber = rand.nextInt(100) + 1;
+            System.out.println("the number is " + randomNumber);
+        while (true) {
+            System.out.println("Enter your guess between (1 - 100): ");
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+            int playerGuess = scanner.nextInt();
+            if (playerGuess == randomNumber) {
+                System.out.println("Correct you Won!");
+                break;
+
+            } else if (playerGuess < randomNumber) {
+                System.out.println("Number is lower. Guess again");
+
+            } else {
+                System.out.println("number is high.Guess again.");
+
+            }
 
 
-
-
+        }
     }
 }
