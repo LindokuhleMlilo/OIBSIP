373df80cf4338a6cbc9748bf809407199a159018
Lindokuhle <144027571+LindokuhleMlilo@users.noreply.github.com>
2024-06-26 15:09:06 +0200

    Add files via upload

---
+++ Number_Guessing_Game/src/main/java/org/Game/Main.java
@@ -0,0 +1,21 @@
+package org.Game;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(100) + 1;
+
+        System.out.println("Enter your guess between (1 - 100)");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
