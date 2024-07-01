f577a99d02578454e05c84cd7a34227aa8641d7a
lindokuhle <lindokuhlemlilo89@gmail.com>
2024-07-01 14:14:44 +0200

    added limit of tries to 5 and to only accept integer

--- Number_Guessing_Game/src/main/java/org/Game/Main.java
+++ Number_Guessing_Game/src/main/java/org/Game/Main.java
@@ -1,34 +1,42 @@
 package org.Game;
+
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
-
         Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
+        int randomNumber = rand.nextInt(101);
+        int count = 0;
+        System.out.println("The number is " + randomNumber);
 
-            int randomNumber = rand.nextInt(100) + 1;
-            System.out.println("the number is " + randomNumber);
         while (true) {
             System.out.println("Enter your guess between (1 - 100): ");
 
-            Scanner scanner = new Scanner(System.in);
-            int playerGuess = scanner.nextInt();
-            if (playerGuess == randomNumber) {
-                System.out.println("Correct you Won!");
-                break;
+            if (scanner.hasNextInt()) {
+                int playerGuess = scanner.nextInt();
+                count++;
 
-            } else if (playerGuess < randomNumber) {
-                System.out.println("Number is lower. Guess again");
-
+                if (playerGuess == randomNumber) {
+                    System.out.println("Correct! You won!");
+                    System.out.println("It took you " + count + " tries to win.");
+                    break;
+                } else if (count == 5) {
+                    System.out.println("You lost. The number was " + randomNumber);
+                    break;
+                } else if (playerGuess < randomNumber) {
+                    System.out.println("The number is higher. Guess again.");
+                } else {
+                    System.out.println("The number is lower. Guess again.");
+                }
             } else {
-                System.out.println("number is high.Guess again.");
-
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next(); // Clear the invalid input
             }
-
-
         }
+
+        scanner.close();
     }
 }
