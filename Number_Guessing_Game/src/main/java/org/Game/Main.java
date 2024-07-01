a0ab052cdbbe74cfec9d4acbda8e1684ac8f4e37
lindokuhle <lindokuhlemlilo89@gmail.com>
2024-07-01 14:45:38 +0200

    added 3 rounds for the player to try

--- Number_Guessing_Game/src/main/java/org/Game/Main.java
+++ Number_Guessing_Game/src/main/java/org/Game/Main.java
@@ -8,34 +8,45 @@ public class Main {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        int randomNumber = rand.nextInt(101);
-        int count = 0;
-        System.out.println("The number is " + randomNumber);
+        int rounds = 3; // Number of rounds the player has
+        int maxAttemptsPerRound = 3; // Maximum attempts per round
+        int count = 0; // Count of guesses within the current round
+        int randomNumber;
 
-        while (true) {
-            System.out.println("Enter your guess between (1 - 100): ");
+        while (rounds > 0) {
+            randomNumber = rand.nextInt(101); // Generate a new random number for each round
+            count = 0;
+            System.out.println("Round " + (4 - rounds) + " - The number is " + randomNumber);
 
-            if (scanner.hasNextInt()) {
-                int playerGuess = scanner.nextInt();
-                count++;
+            while (count < maxAttemptsPerRound) {
+                System.out.println("Enter your guess between (1 - 100): ");
 
-                if (playerGuess == randomNumber) {
-                    System.out.println("Correct! You won!");
-                    System.out.println("It took you " + count + " tries to win.");
-                    break;
-                } else if (count == 5) {
-                    System.out.println("You lost. The number was " + randomNumber);
-                    break;
-                } else if (playerGuess < randomNumber) {
-                    System.out.println("The number is higher. Guess again.");
+                if (scanner.hasNextInt()) {
+                    int playerGuess = scanner.nextInt();
+                    count++;
+
+                    if (playerGuess == randomNumber) {
+                        System.out.println("Correct! You won this round!");
+                        System.out.println("It took you " + count + " tries to win this round.");
+                        break;
+                    } else if (count == maxAttemptsPerRound) {
+                        System.out.println("You lost this round. The number was " + randomNumber);
+                        break;
+                    } else if (playerGuess < randomNumber) {
+                        System.out.println("The number is higher. Guess again.");
+                    } else {
+                        System.out.println("The number is lower. Guess again.");
+                    }
                 } else {
-                    System.out.println("The number is lower. Guess again.");
+                    System.out.println("Invalid input. Please enter an integer.");
+                    scanner.next();
                 }
-            } else {
-                System.out.println("Invalid input. Please enter an integer.");
-                scanner.next(); // Clear the invalid input
             }
+
+            rounds--;
         }
+
+        System.out.println("You've used all your rounds. Game over!");
 
         scanner.close();
     }
